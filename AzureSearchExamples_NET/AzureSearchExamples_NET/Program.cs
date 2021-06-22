3bbeb038429e11e63e6a8cc86fdd51f77f2d4cbd
Brian C Harrison <brian_c_harrison@hotmail.com>
2021-06-22 16:38:42 -0400

    Initial

---
+++ AzureSearchExamples_NET/AzureSearchExamples_NET/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureSearchExamples_NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
